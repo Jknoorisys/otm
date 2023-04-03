2fba7078ec34ad248428ceb90f0fbb47a172867c
Jknoorisys <javeriya.noorisys@gmail.com>
2023-04-03 09:42:27 +0530

    rework

--- application/controllers/Admin_Report.php
+++ application/controllers/Admin_Report.php
@@ -1,13 +1,36 @@
 <?php
 class Admin_Report extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+			$this->load->model('Admin_Model');
+			// $this->load->model('manager_model');
+
+    }
     public function add_questions()
     {
+        // $data['grp'] = $this->Admin_Model->group_id();
+        // echo json_encode($grp['month_start']);exit;
         $this->load->view('admin/admin_header');
 	    $this->load->view('admin/admin_menubar');
         $this->load->view('admin-report/admin_add_questions');
 	    $this->load->view('admin/admin_footer');
 
+    }
+    public function add_que_func()
+    {
+        $question = array(
+            'question' => $this->input->post('quet'),
+            'user_group_id' => $this->input->post('user_group_id'),
+            'created_at' => date('d-m-Y h:i:sa')
+        );
+        echo json_encode($question);exit;
+        $this->Admin_Model->add_que_func($question);
+        $this->load->view('admin/admin_header');
+	    $this->load->view('admin/admin_menubar');
+        $this->load->view('admin-report/admin_add_questions');
+	    $this->load->view('admin/admin_footer');
     }
 
 
--- application/models/Admin_Model.php
+++ application/models/Admin_Model.php
@@ -1,7 +1,17 @@
 <?php
 
-    class Admin_Model extends CI_Model{
-
+    class Admin_Model extends CI_Model
+    {
+        public function add_que_func($question)
+        {
+            $this->db->insert('questions',$question);
+            return  $this->db->insert();
+        }
+        public function group_id()
+        {
+            $grp = $this->db->from('question_groups')->get()->result_array();
+            return $grp;
+        }
     }
     
 ?>
